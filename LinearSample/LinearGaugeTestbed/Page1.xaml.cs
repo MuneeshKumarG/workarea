aedc7609b66a87ffd11685648f2b19e693067f87
Muneesh Kumar <muneeshg@syncfusion.com>
2022-01-21 23:01:32 +0530

    Changes committed

--- LinearSample/LinearGaugeTestbed/Page1.xaml.cs
+++ LinearSample/LinearGaugeTestbed/Page1.xaml.cs
@@ -10,6 +10,22 @@ namespace LinearGaugeTestbed
 {
 	public partial class Page1 : ContentPage
 	{
+        private double position = 10;
+
+        public double Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public Page1()
+		{
+			InitializeComponent();
+            this.BindingContext = this;
+
+        }
+
+
         public List<string> ColorCollection
         {
             get
@@ -26,13 +42,6 @@ namespace LinearGaugeTestbed
             }
         }
         LinearGradientBrush linearGradientBrush = new LinearGradientBrush();
-
-        public Page1()
-		{
-			InitializeComponent();
-            this.BindingContext = this;
-
-        }
 
         private void MinimumIncrease_Clicked(object sender, EventArgs e)
         {
@@ -158,15 +167,15 @@ namespace LinearGaugeTestbed
             string color = (sender as Picker).SelectedItem.ToString();
 
             if (color == "Red")
-                gauge.AxisLineStyle.Fill = new SolidColorBrush(Colors.Red);
+                gauge.LineStyle.Fill = new SolidColorBrush(Colors.Red);
             else if (color == "Black")
-                gauge.AxisLineStyle.Fill = new SolidColorBrush(Colors.Black);
+                gauge.LineStyle.Fill = new SolidColorBrush(Colors.Black);
             else if (color == "Green")
-                gauge.AxisLineStyle.Fill = new SolidColorBrush(Colors.Green);
+                gauge.LineStyle.Fill = new SolidColorBrush(Colors.Green);
             else if (color == "Blue")
-                gauge.AxisLineStyle.Fill = new SolidColorBrush(Colors.Blue);
+                gauge.LineStyle.Fill = new SolidColorBrush(Colors.Blue);
             else if (color == "Transparent")
-                gauge.AxisLineStyle.Fill = new SolidColorBrush(Colors.Transparent);
+                gauge.LineStyle.Fill = new SolidColorBrush(Colors.Transparent);
             else if (color == "Gradient")
             {
                 linearGradientBrush.StartPoint = new Point(0, 0.5);
@@ -174,7 +183,7 @@ namespace LinearGaugeTestbed
                 linearGradientBrush.GradientStops.Add(new Microsoft.Maui.Controls.GradientStop(Colors.Red, 0.25f));
                 linearGradientBrush.GradientStops.Add(new Microsoft.Maui.Controls.GradientStop(Colors.Green, 0.75f));
 
-                gauge.AxisLineStyle.Fill = linearGradientBrush;
+                gauge.LineStyle.Fill = linearGradientBrush;
             }
         }
 
@@ -197,15 +206,15 @@ namespace LinearGaugeTestbed
             string radius = (sender as Picker).SelectedItem.ToString();
 
             if (radius == "0,5,5,5")
-                gauge.AxisLineStyle.CornerRadius = new Thickness(0, 5, 5, 5);
+                gauge.LineStyle.CornerRadius = new Thickness(0, 5, 5, 5);
             else if (radius == "5,0,5,5")
-                gauge.AxisLineStyle.CornerRadius = new Thickness(5, 0, 5, 5);
+                gauge.LineStyle.CornerRadius = new Thickness(5, 0, 5, 5);
             else if (radius == "5,5,0,5")
-                gauge.AxisLineStyle.CornerRadius = new Thickness(5, 5, 0, 5);
+                gauge.LineStyle.CornerRadius = new Thickness(5, 5, 0, 5);
             else if (radius == "5,5,5,0")
-                gauge.AxisLineStyle.CornerRadius = new Thickness(5, 5, 5, 0);
+                gauge.LineStyle.CornerRadius = new Thickness(5, 5, 5, 0);
             else if (radius == "5,5,5,5")
-                gauge.AxisLineStyle.CornerRadius = new Thickness(5, 5, 5, 5);
+                gauge.LineStyle.CornerRadius = new Thickness(5, 5, 5, 5);
         }
 
         public Array CornerStyles
@@ -225,9 +234,19 @@ namespace LinearGaugeTestbed
             }
 
         }
+
+        public Array ShapeTypes
+        {
+            get
+            {
+                return Enum.GetValues(typeof(ShapeType));
+            }
+
+        }
+
         private void Picker_SelectedIndexChanged3(object sender, EventArgs e)
         {
-            gauge.AxisLineStyle.CornerStyle = (CornerStyle)(sender as Picker).SelectedItem;
+            gauge.LineStyle.CornerStyle = (CornerStyle)(sender as Picker).SelectedItem;
         }
 
         public List<string> DashArrayCollection
@@ -247,21 +266,21 @@ namespace LinearGaugeTestbed
             string dashArray = (sender as Picker).SelectedItem.ToString();
 
             if (dashArray == "2,2")
-                gauge.AxisLineStyle.DashArray = new DoubleCollection() {2,2 };
+                gauge.LineStyle.DashArray = new DoubleCollection() {2,2 };
             else if (dashArray == "4,4")
-                gauge.AxisLineStyle.DashArray = new DoubleCollection() { 4,4};
+                gauge.LineStyle.DashArray = new DoubleCollection() { 4,4};
             else if (dashArray == "Null")
-                gauge.AxisLineStyle.DashArray = null;
+                gauge.LineStyle.DashArray = null;
         }
 
         private void AxisLineThicknessIncrease_Clicked(object sender, EventArgs e)
         {
-            gauge.AxisLineStyle.Thickness += 1;
+            gauge.LineStyle.Thickness += 1;
         }
 
         private void AxisLineThicknessDecrease_Clicked(object sender, EventArgs e)
         {
-            gauge.AxisLineStyle.Thickness -= 1;
+            gauge.LineStyle.Thickness -= 1;
         }
 
         private void Picker_SelectedIndexChanged5(object sender, EventArgs e)
@@ -387,12 +406,12 @@ namespace LinearGaugeTestbed
 
         private void AxisLabelFontSizeIncrease_Clicked(object sender, EventArgs e)
         {
-            gauge.AxisLabelStyle.FontSize += 1;
+            gauge.LabelStyle.FontSize += 1;
         }
 
         private void AxisLabelFontSizeDecrease_Clicked(object sender, EventArgs e)
         {
-            gauge.AxisLabelStyle.FontSize -= 1;
+            gauge.LabelStyle.FontSize -= 1;
         }
 
         private void Picker_SelectedIndexChanged9(object sender, EventArgs e)
@@ -411,13 +430,13 @@ namespace LinearGaugeTestbed
             else if (color == "Transparent")
                 brush =Colors.Transparent;
 
-            gauge.AxisLabelStyle.TextColor = brush;
+            gauge.LabelStyle.TextColor = brush;
         }
 
 
         private void Picker_SelectedIndexChanged10(object sender, EventArgs e)
         {
-            gauge.AxisLabelStyle.FontAttributes = (FontAttributes)(sender as Picker).SelectedItem;
+            gauge.LabelStyle.FontAttributes = (FontAttributes)(sender as Picker).SelectedItem;
         }
 
 
@@ -501,6 +520,58 @@ namespace LinearGaugeTestbed
            // barPointer.Stroke = brush;
         }
 
+        private void Picker_SelectedIndexChanged15(object sender, EventArgs e)
+        {
+            shapePointer.ShapeType = (ShapeType)(sender as Picker).SelectedItem;
+        }
+
+        private void Picker_SelectedIndexChanged16(object sender, EventArgs e)
+        {
+            string color = (sender as Picker).SelectedItem.ToString();
+            Brush brush = null;
+
+            if (color == "Red")
+                brush = new SolidColorBrush(Colors.Red);
+            else if (color == "Black")
+                brush = new SolidColorBrush(Colors.Black);
+            else if (color == "Green")
+                brush = new SolidColorBrush(Colors.Green);
+            else if (color == "Blue")
+                brush = new SolidColorBrush(Colors.Blue);
+            else if (color == "Transparent")
+                brush = new SolidColorBrush(Colors.Transparent);
+            else if (color == "Gradient")
+            {
+                linearGradientBrush.StartPoint = new Point(0, 0.5);
+                linearGradientBrush.EndPoint = new Point(1, 0.5);
+                linearGradientBrush.GradientStops.Add(new Microsoft.Maui.Controls.GradientStop(Colors.Red, 0.25f));
+                linearGradientBrush.GradientStops.Add(new Microsoft.Maui.Controls.GradientStop(Colors.Green, 0.75f));
+
+                brush = linearGradientBrush;
+            }
+
+            shapePointer.Fill = brush;
+        }
+
+        private void Picker_SelectedIndexChanged17(object sender, EventArgs e)
+        {
+            string color = (sender as Picker).SelectedItem.ToString();
+            Color brush = null;
+
+            if (color == "Red")
+                brush = Colors.Red;
+            else if (color == "Black")
+                brush = Colors.Black;
+            else if (color == "Green")
+                brush = Colors.Green;
+            else if (color == "Blue")
+                brush = Colors.Blue;
+            else if (color == "Transparent")
+                brush = Colors.Transparent;
+
+             shapePointer.Stroke = brush;
+        }
+
         private void AxisLineGradientStopsSet_Clicked(object sender, EventArgs e)
         {
             var gradientStops = new System.Collections.ObjectModel.ObservableCollection<GaugeGradientStop>();
@@ -510,13 +581,13 @@ namespace LinearGaugeTestbed
             gradientStops.Add(new GaugeGradientStop { Value = 50, Color = Colors.Blue });
             gradientStops.Add(new GaugeGradientStop { Value = 100, Color = Colors.Green });
 
-           gauge.AxisLineStyle.GradientStops = gradientStops;
+           gauge.LineStyle.GradientStops = gradientStops;
 
         }
 
         private void AxisLineGradientStopsSetNull_Clicked(object sender, EventArgs e)
         {
-            gauge.AxisLineStyle.GradientStops = null;
+            gauge.LineStyle.GradientStops = null;
         }
 
         private void RangeStartIncrease_Clicked(object sender, EventArgs e)
@@ -656,6 +727,265 @@ namespace LinearGaugeTestbed
         private void BarPointerGradientStopsSetNull_Clicked(object sender, EventArgs e)
         {
             barPointer.GradientStops = null;
+        }
+
+        private void RangeChildSet_Clicked(object sender, EventArgs e)
+        {
+            range.Child = new Label()
+            {
+                Text ="Dynamic"
+            };
+        }
+
+        private void RangeChildSetNull_Clicked(object sender, EventArgs e)
+        {
+            range.Child = null;
+        }
+
+        private void RangeAdd_Clicked(object sender, EventArgs e)
+        {
+            gauge.Ranges.Add(new LinearRange()
+            {
+                StartValue = 30,
+                EndValue =60,
+                Fill=new SolidColorBrush(Colors.Red),
+            });
+        }
+
+        private void RangeInsert_Clicked(object sender, EventArgs e)
+        {
+            gauge.Ranges.Insert(0,new LinearRange()
+            {
+                StartValue = 30,
+                EndValue = 60,
+                Fill = new SolidColorBrush(Colors.Yellow),
+            });
+        }
+
+        private void RangeClear_Clicked(object sender, EventArgs e)
+        {
+            gauge.Ranges.Clear();
+        }
+
+        private void BarPointerChildSet_Clicked(object sender, EventArgs e)
+        {
+            barPointer.Child = new Label()
+            {
+                Text = "Dynamic",
+                Background = new SolidColorBrush(Colors.Yellow),
+            };
+        }
+
+        private void BarPointerChildSetNull_Clicked(object sender, EventArgs e)
+        {
+            barPointer.Child = null;
+        }
+
+        private void BarPointerAdd_Clicked(object sender, EventArgs e)
+        {
+            gauge.BarPointers.Add(new BarPointer()
+            {
+                Value = 80,
+                Fill = new SolidColorBrush(Colors.Red),
+            });
+        }
+
+        private void BarPointerInsert_Clicked(object sender, EventArgs e)
+        {
+            gauge.BarPointers.Insert(0, new BarPointer()
+            {
+                Value = 80,
+                Fill = new SolidColorBrush(Colors.Yellow),
+            });
+        }
+
+        private void BarPointerClear_Clicked(object sender, EventArgs e)
+        {
+            gauge.BarPointers.Clear();
+        }
+
+        private void ShapePointerValueIncrease_Clicked(object sender, EventArgs e)
+        {
+            shapePointer.Value += 1;
+        }
+
+        private void ShapePointerValueDecrease_Clicked(object sender, EventArgs e)
+        {
+            shapePointer.Value -= 1;
+        }
+
+        private void ShapePointerOffsetLeftIncrease_Clicked(object sender, EventArgs e)
+        {
+            UpdateOffset(shapePointer, 1, 0);
+        }
+
+        private void ShapePointerOffseLeftDecrease_Clicked(object sender, EventArgs e)
+        {
+            UpdateOffset(shapePointer, -1, 0);
+        }
+
+        private void ShapePointerOffsetTopIncrease_Clicked(object sender, EventArgs e)
+        {
+            UpdateOffset(shapePointer, 0, 1);
+        }
+
+        private void ShapePointerOffseTopDecrease_Clicked(object sender, EventArgs e)
+        {
+            UpdateOffset(shapePointer, 0, -1);
+        }
+
+        private void UpdateOffset(LinearMarkerPointer pointer, double left, double top)
+        {
+            if (pointer.OffsetPoint == null)
+                pointer.OffsetPoint = new Point(0, 0);
+
+            pointer.OffsetPoint = new Point(pointer.OffsetPoint.X + left, pointer.OffsetPoint.Y + top);
+        }
+
+        private void BarPositionHAlignmentStart_Clicked(object sender, EventArgs e)
+        {
+            UpdateHorizontalAlignment(shapePointer, GaugeAlignment.Start);
+        }
+
+        private void BarPositionHAlignmentCenter_Clicked(object sender, EventArgs e)
+        {
+            UpdateHorizontalAlignment(shapePointer, GaugeAlignment.Center);
+        }
+
+        private void BarPositionHAlignmentEnd_Clicked(object sender, EventArgs e)
+        {
+            UpdateHorizontalAlignment(shapePointer, GaugeAlignment.End);
+        }
+
+        private void UpdateHorizontalAlignment(LinearMarkerPointer pointer, GaugeAlignment gaugeAlignment)
+        {
+            pointer.HorizontalAlignment= gaugeAlignment;
+        }
+
+        private void BarPositionVAlignmentStart_Clicked(object sender, EventArgs e)
+        {
+            UpdateVerticalAlignment(shapePointer, GaugeAlignment.Start);
+        }
+
+        private void BarPositionVAlignmentCenter_Clicked(object sender, EventArgs e)
+        {
+            UpdateVerticalAlignment(shapePointer, GaugeAlignment.Center);
+        }
+
+        private void BarPositionVAlignmentEnd_Clicked(object sender, EventArgs e)
+        {
+            UpdateVerticalAlignment(shapePointer, GaugeAlignment.End);
+        }
+
+        private void UpdateVerticalAlignment(LinearMarkerPointer pointer, GaugeAlignment gaugeAlignment)
+        {
+            pointer.VerticalAlignment = gaugeAlignment;
+        }
+
+        private void ShapePointerHeightIncrease_Clicked(object sender, EventArgs e)
+        {
+            shapePointer.ShapeHeight += 1;
+        }
+
+        private void ShapePointerHeightDecrease_Clicked(object sender, EventArgs e)
+        {
+            shapePointer.ShapeHeight -= 1;
+        }
+
+        private void ShapePointerWidthIncrease_Clicked(object sender, EventArgs e)
+        {
+            shapePointer.ShapeWidth += 1;
+        }
+
+        private void ShapePointerWidthDecrease_Clicked(object sender, EventArgs e)
+        {
+            shapePointer.ShapeWidth -= 1;
+        }
+
+        private void ShapePointerBorderIncrease_Clicked(object sender, EventArgs e)
+        {
+            shapePointer.StrokeThickness += 1;
+        }
+
+        private void ShapePointerBorderDecrease_Clicked(object sender, EventArgs e)
+        {
+            shapePointer.StrokeThickness -= 1;
+        }
+
+
+        private void ContentPointerValueIncrease_Clicked(object sender, EventArgs e)
+        {
+            contentPointer.Value += 1;
+        }
+
+        private void ContentPointerValueDecrease_Clicked(object sender, EventArgs e)
+        {
+            contentPointer.Value -= 1;
+        }
+
+        private void ContentPointerOffsetLeftIncrease_Clicked(object sender, EventArgs e)
+        {
+            UpdateOffset(contentPointer, 1, 0);
+        }
+
+        private void ContentPointerOffseLeftDecrease_Clicked(object sender, EventArgs e)
+        {
+            UpdateOffset(contentPointer, -1, 0);
+        }
+
+        private void ContentPointerOffsetTopIncrease_Clicked(object sender, EventArgs e)
+        {
+            UpdateOffset(contentPointer, 0, 1);
+        }
+
+        private void ContentPointerOffseTopDecrease_Clicked(object sender, EventArgs e)
+        {
+            UpdateOffset(contentPointer, 0, -1);
+        }
+
+
+        private void ContentPositionHAlignmentStart_Clicked(object sender, EventArgs e)
+        {
+            UpdateHorizontalAlignment(contentPointer, GaugeAlignment.Start);
+        }
+
+        private void ContentPositionHAlignmentCenter_Clicked(object sender, EventArgs e)
+        {
+            UpdateHorizontalAlignment(contentPointer, GaugeAlignment.Center);
+        }
+
+        private void ContentPositionHAlignmentEnd_Clicked(object sender, EventArgs e)
+        {
+            UpdateHorizontalAlignment(contentPointer, GaugeAlignment.End);
+        }
+
+        private void ContentPositionVAlignmentStart_Clicked(object sender, EventArgs e)
+        {
+            UpdateVerticalAlignment(contentPointer, GaugeAlignment.Start);
+        }
+
+        private void ContentPositionVAlignmentCenter_Clicked(object sender, EventArgs e)
+        {
+            UpdateVerticalAlignment(contentPointer, GaugeAlignment.Center);
+        }
+
+        private void ContentPositionVAlignmentEnd_Clicked(object sender, EventArgs e)
+        {
+            UpdateVerticalAlignment(contentPointer, GaugeAlignment.End);
+        }
+
+        private void ContentPointerChildSet_Clicked(object sender, EventArgs e)
+        {
+            contentPointer.Content = new Label()
+            {
+                Text = "Dynamic",
+                Background = new SolidColorBrush(Colors.Yellow),
+            };
+        }
+
+        private void ContentPointerChildSetNull_Clicked(object sender, EventArgs e)
+        {
+            contentPointer.Content = null;
         }
 
     }
