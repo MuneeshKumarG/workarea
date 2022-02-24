86bbd27f4bdbde7b4579db29b7d3f7b4a8bfed4c
Muneesh Kumar <muneeshg@syncfusion.com>
2022-02-24 20:03:18 +0530

    Changes committed

--- SB/SBSample/SBSample/Page4.xaml.cs
+++ SB/SBSample/SBSample/Page4.xaml.cs
@@ -25,14 +25,6 @@ public partial class Page4 : ContentPage, INotifyPropertyChanged
 		Size = width / 4;
 	}
 
-	public Page4()
-	{
-		InitializeComponent();
-	}
-
-
-
-
 	public event PropertyChangedEventHandler PropertyChanged;
 
 	// This method is called by the Set accessor of each property.  
@@ -43,5 +35,81 @@ public partial class Page4 : ContentPage, INotifyPropertyChanged
 		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 	}
 
+
+
+	public Page4()
+	{
+		InitializeComponent();
+		BindingContext = this;
+	}
+
+    private void SfLinearGauge_LabelCreated(object sender, LabelCreatedEventArgs e)
+    {
+		e.Text = e.Text + " cm";
+    }
+
+    private double heightPointerValue = 130;
+
+    public double HeightPointerValue
+    {
+        get { return heightPointerValue; }
+        set { heightPointerValue = value; NotifyPropertyChanged(); }
+    }
+
+    private void Pointer1_ValueChanged(object sender, Syncfusion.Maui.Gauges.ValueChangedEventArgs e)
+    {
+		HeightPointerValue = e.Value;
+		heightAnnotationLabel.Text = (int)HeightPointerValue + " cm";
+	}
+
+
+
+    private double waterLevelPointerValue = 150;
+
+    public double WaterLavelPointerValue
+    {
+        get { return waterLevelPointerValue; }
+        set { waterLevelPointerValue = value; NotifyPropertyChanged(); }
+    }
+
+	private void Pointer1_ValueChanged1(object sender, Syncfusion.Maui.Gauges.ValueChangedEventArgs e)
+	{
+		WaterLavelPointerValue = e.Value;
+		waterLevelAnnotationLabel.Text = ((int)WaterLavelPointerValue).ToString()+" ml";
+	}
+
+    
+}
+
+public class TaskTrackerGauge :SfLinearGauge
+{
+	public override List<GaugeLabelInfo> GenerateVisibleLabels()
+	{
+		List<GaugeLabelInfo> customLabels = new List<GaugeLabelInfo>();
+		customLabels.Add(new GaugeLabelInfo { Value = 0, Text = "Mar 19" });
+		customLabels.Add(new GaugeLabelInfo { Value = 20, Text = "Jul 19" });
+		customLabels.Add(new GaugeLabelInfo { Value = 40, Text = "Oct 19" });
+		customLabels.Add(new GaugeLabelInfo { Value = 60, Text = "Jan 20" });
+		customLabels.Add(new GaugeLabelInfo { Value = 80, Text = "Apr 20" });
+		customLabels.Add(new GaugeLabelInfo { Value = 100, Text = "Jul 20" });
+
+		return customLabels;
+
+	}
+}
+
+public class SleepWatchScoreGauge : SfLinearGauge
+{
+	public override List<GaugeLabelInfo> GenerateVisibleLabels()
+	{
+		List<GaugeLabelInfo> customLabels = new List<GaugeLabelInfo>();
+		customLabels.Add(new GaugeLabelInfo { Value = 150, Text = "Poor" });
+		customLabels.Add(new GaugeLabelInfo { Value = 250, Text = "Fair" });
+		customLabels.Add(new GaugeLabelInfo { Value = 350, Text = "Good" });
+		customLabels.Add(new GaugeLabelInfo { Value = 450, Text = "Excellent" });
+
+		return customLabels;
+
+	}
 }
 
